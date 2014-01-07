e17de27ec4547bec48f0e49dd6443aaab51727f8
Srikanth <srikanth.kunkulagunta@gmail.com>
2014-01-07 23:29:56 +0000

    finished naive implementation of quiz edit action

--- app/controllers/quizzes.rb
+++ app/controllers/quizzes.rb
@@ -36,8 +36,7 @@ class QuizApp < Sinatra::Base
     if @quiz.save
       redirect to '/quizzes'
     else
-      flash.now[:errors] = ["Sorry,
-      your Quiz was unable to save. Please try again"]
+      flash.now[:errors] = ["Sorry, your Quiz was unable to save. Please try again"]
       haml :"quizzes/new"
     end
   end
@@ -45,9 +44,34 @@ class QuizApp < Sinatra::Base
   get '/quizzes/:id/edit' do
     if session[:admin_id]
       @quiz = Quiz.get(params[:id])
+      questions = @quiz.questions
+      @questions = questions.map {|q| {q => q.answers} }
       haml :"quizzes/edit"
     else
       redirect to '/sessions/new'
+    end
+  end
+
+  post '/quizzes/:id/edit' do
+    @quiz = Quiz.get(params[:id])
+    @quiz.update(:title => params[:title])
+    hash = params[:question]
+    hash.each do |key, value|
+      text = value["question_text"]
+      @question = Question.get(key.to_i)
+      @question.update(:question_text => text)
+      answers = value["answer"]
+      answers.each do |key, value|
+        @answer = Answer.get(key.to_i)
+        r = value["response"]
+        @answer.update(:response => r)
+      end
+    end
+    if @quiz.save
+      redirect to '/quizzes'
+    else
+      flash.now[:errors] = ["Sorry,your Quiz was unable to save. Please try again"]
+      haml :"quizzes/edit"
     end
   end
 end
