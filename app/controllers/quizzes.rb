47b14cd2acda485fdb86883734f3aafd45b87608
Srikanth <srikanth.kunkulagunta@gmail.com>
2014-01-07 15:00:46 +0000

    THE FORM WORKS. HALLELUJAH.

--- app/controllers/quizzes.rb
+++ app/controllers/quizzes.rb
@@ -1,38 +1,45 @@
 class QuizApp < Sinatra::Base
 
-	get '/quizzes' do
-		if session[:admin_id]
-			@quizzes = Quiz.all
-			haml :"quizzes/index"
-		else
-			redirect to '/sessions/new'
-		end
-	end
+  get '/quizzes' do
+    if session[:admin_id]
+      @quizzes = Quiz.all
+      haml :"quizzes/index"
+    else
+      redirect to '/sessions/new'
+    end
+  end
 
-	get '/quizzes/new' do
-		if session[:admin_id]
-			@quiz = Quiz.new
-			haml :"quizzes/new"
-		else
-			redirect to '/sessions/new'
-		end
-	end
+  get '/quizzes/new' do
+    if session[:admin_id]
+      @quiz = Quiz.new
+      haml :"quizzes/new"
+    else
+      redirect to '/sessions/new'
+    end
+  end
 
-	post '/quizzes/new' do
-		@quiz = Quiz.new(:title => params[:title])
-		@quiz.questions = params[:question]
-		@quiz.questions.each do |q|
-			q.answers = params[:answer].map {|a| Answer.new(response: a["response"]) }
-		end
-		# @quiz = params[:quiz]
-		# raise "#{@quiz.questions}"
-		if @quiz.save
-			redirect to '/quizzes'
-		else
-			flash.now[:errors] = ["Sorry,
-			your Quiz was unable to save. Please try again"]
-			haml :"quizzes/new"
-		end
-	end
-		
+  post '/quizzes/new' do
+    @quiz = Quiz.new(:title => params[:title])
+
+    hash = params[:question]
+    hash.each do |key, value|
+      text = value["question_text"]
+      @q = Question.new(question_text: text)
+      answers = value["answer"]
+      answers.each do |key, value|
+        r = value["response"]
+        value["correctness"] == "on" ? c = true : c = false
+        @q.answers << Answer.new(:response => r, :correctness => c)
+      end
+      @quiz.questions << @q
+    end
+    if @quiz.save
+      redirect to '/quizzes'
+    else
+      flash.now[:errors] = ["Sorry,
+      your Quiz was unable to save. Please try again"]
+      haml :"quizzes/new"
+    end
+  end
+    
 end
