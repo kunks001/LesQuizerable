44be93f91fe5fdf8ee3a0b0ac8051f72cbd95f1d
Srikanth <srikanth.kunkulagunta@gmail.com>
2014-01-08 00:54:38 +0000

    played about with admin signup tests

--- spec/features/admins/admin_signup_spec.rb
+++ spec/features/admins/admin_signup_spec.rb
@@ -1,34 +1,39 @@
 require 'spec_helper'
 
 feature "Creating a new admin" do
+
+  before(:each) do
+    FactoryGirl.create(:admin)
+  end
+
+  after(:each) do
+    reset_session
+  end
   
   scenario "when logged out" do
     visit '/admins/new'
     expect(current_path).to eq '/sessions/new'        
   end
 
-  context "when logged in" do
+  ### TODO: refactor signin lambda duplicates
 
-    before(:each) do
-      FactoryGirl.create(:admin)
-      sign_in('test@test.com','foobar')
-    end
+  scenario "when logged in" do
+    sign_in('test@test.com','foobar')  
+    lambda { sign_up }.should change(Admin, :count).by(1)
+  end
 
-    after(:each) do
-      reset_session
-    end
+  scenario "when logged in, but with a password that doesn't match" do
+    sign_in('test@test.com','foobar')
+    lambda { sign_up('a@a.com', 'pass', 'wrong') }.should change(Admin, :count).by(0)
+    expect(current_path).to eq('/admins/new')   
+    expect(page).to have_content("Password does not match the confirmation")
+  end
 
-    scenario "with a password that doesn't match" do
-      lambda { sign_up('a@a.com', 'pass', 'wrong') }.should change(Admin, :count).by(0)
-      expect(current_path).to eq('/admins/new')   
-      expect(page).to have_content("Password does not match the confirmation")
-    end
-
-    scenario "with an email that is already registered" do    
-      lambda { sign_up }.should change(Admin, :count).by(1)
-      lambda { sign_up }.should change(Admin, :count).by(0)
-      expect(page).to have_content("The email you have entered is already taken")
-    end
+  scenario "when logged in, with an email that is already registered" do 
+    sign_in('test@test.com','foobar')   
+    lambda { sign_up }.should change(Admin, :count).by(1)
+    lambda { sign_up }.should change(Admin, :count).by(0)
+    expect(page).to have_content("The email you have entered is already taken")
   end
 
 end
