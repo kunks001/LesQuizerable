3c67c98cafe574673178ba61e031791b2334734a
Srikanth <srikanth.kunkulagunta@gmail.com>
2014-01-14 21:16:56 +0000

    more refactoring for new quiz method

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -7,49 +7,49 @@ class Quiz
 	property :id, Serial
   property :title, String
 
-  def upload(value)
+  def upload_image(value)
     file  = value["file"][:tempfile]
     filename = value["file"][:filename]
     ApplicationHelper::upload(file, filename)
   end
 
+  def create_image(object,value)
+    upload_image(value)
+    object.image = Image.create(:filename => value["file"][:filename])
+  end
+
+  def correct?(value)
+    value == "on" ? c = true : c = false
+  end
+
+  def create_answers(q,answers)
+    answers.each do |key, value|
+      correctness = correct?(value[:correctness])
+      a = Answer.new(:response => value["response"], :correctness => correctness)
+      create_image(a,value) if value["file"]
+      q.answers << a
+    end
+  end
 
   def save_questions_and_answers(hash,quiz)
     hash.each do |key, value|
-      text = value["question_text"]
-      @q = Question.new(question_text: text)
-      if value["file"]
-        upload(value)
-        @q.image = Image.create(:filename => value["file"][:filename])
-      end
-      answers = value["answer"]
-      answers.each do |key, value|
-        r = value["response"]
-        value["correctness"] == "on" ? c = true : c = false
-        @a = Answer.new(:response => r, :correctness => c)
-        if value["file"]
-          upload(value)
-          @a.image = Image.create(:filename => value["file"][:filename])
-        end
-        @q.answers << @a
-      end
-      quiz.questions << @q
+      q = Question.new(question_text: value["question_text"])
+      create_image(q,value) if value["file"]
+      create_answers(q,value["answer"])
+      quiz.questions << q
     end
   end
 
   def edit_questions_and_answers(hash,quiz)
     hash.each do |key, value|
-      text = value["question_text"]
       @q = Question.get(key.to_i)
-      @q.update(:question_text => text)
+      @q.update(:question_text => value["question_text"])
       if value["file"]
-        file  = value["file"][:tempfile]
-        filename = value["file"][:filename]
-        ApplicationHelper::upload(file, filename)
+        upload_image(value)
         if @q.image
        		@q.image.update(:filename => filename)
        	else
-       		@q.image = Image.create(:filename => filename)
+       		@q.image = Image.create(:filename => value["file"][:filename])
        	end
       end
       if @q.save
@@ -58,9 +58,7 @@ class Quiz
 	        @answer = Answer.get(key.to_i)
 	        r = value["response"]
 	        if value["file"]
-	          file  = value["file"][:tempfile]
-	          filename = value["file"][:filename]
-	          ApplicationHelper::upload(file, filename)
+	          upload_image(value)
 	          if @answer.image
 	            @answer.image.update(:filename => filename)
 	            @answer.save
